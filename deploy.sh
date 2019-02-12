a01aa038eba30b73d4f0ee58d5f5db0cd00f5bea
jokerbj <jokerbj@126.com>
2019-02-13 02:45:38 +0800

    del save theme

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# GIT全局配置
+git config user.name "jokerbj"
+git config user.email "849185108@qq.com"
+pwd
+ls
+cd _post
+ls
+#it commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+#it push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
